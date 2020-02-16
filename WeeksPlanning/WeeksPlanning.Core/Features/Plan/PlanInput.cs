3fd041d7eb3764bb1b296ad4c3037f136f3c1352
Musta Mohamed <mustafa.mohamed.wd@gmail.com>
2020-02-16 19:20:57 +0200

    refactor PlanInput to accept updating functionality

--- WeeksPlanning/WeeksPlanning.Core/Features/Plan/PlanInput.cs
+++ WeeksPlanning/WeeksPlanning.Core/Features/Plan/PlanInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Framework.Interfaces;
+using WeeksPlanning.Entity;
 using WeeksPlanning.Entity.EntityClasses;
 
 namespace WeeksPlanning.Core.Features.Plan
@@ -7,36 +8,45 @@ namespace WeeksPlanning.Core.Features.Plan
     [Serializable]
     public class PlanInput : IToEntity<PlanEntity>
     {
-
         public long? Id { get; set; }
-        public string PlanName { get; set; }
-        public short PlanDurationInWeeks { get; set; }
-        public DateTime? StatingDateUtc { get; set; }
+        public string Name { get; set; }
+        public short? DurationInWeeks { get; set; }
+        public DateTime? StartingDateUtc { get; set; }
         public long? CreatedByUserId { get; set; }
-        public long? ModifiedByUserId { get; set; }
+        public long? LastModifiedByUserId { get; set; }
 
         public PlanEntity ToEntity()
         {
-            if (Id == null)
+            var entity = new PlanEntity();
+
+            if (Id != null)
             {
-                return new PlanEntity()
-                {
-                    Name = PlanName,
-                    DurationInWeeks = PlanDurationInWeeks,
-                    StartingDateUtc = StatingDateUtc ?? DateTime.UtcNow,
-                    DateCreatedUtc = DateTime.UtcNow,
-                    CreatedByUserId = CreatedByUserId,
-                };
+                entity.Id = Id.GetValueOrDefault();
+                entity.IsNew = false;
             }
-            return new PlanEntity()
+
+            if (!string.IsNullOrEmpty(Name))
+                entity.Name = Name;
+            
+            if (DurationInWeeks != null)
+                entity.DurationInWeeks = DurationInWeeks.GetValueOrDefault();
+            
+            if (StartingDateUtc != null)
+                entity.StartingDateUtc = StartingDateUtc.GetValueOrDefault();
+
+            if (CreatedByUserId != null)
             {
-                Id = Id.GetValueOrDefault(),
-                Name = PlanName,
-                DurationInWeeks = PlanDurationInWeeks,
-                StartingDateUtc = StatingDateUtc ?? DateTime.UtcNow,
-                CreatedByUserId = CreatedByUserId,
-                LastModifiedByUserId = ModifiedByUserId
-            };
+                entity.CreatedByUserId = CreatedByUserId.GetValueOrDefault();
+                entity.DateCreatedUtc = DateTime.UtcNow;
+            }
+
+            if (LastModifiedByUserId != null)
+            {
+                entity.LastModifiedByUserId = LastModifiedByUserId.GetValueOrDefault();
+                entity.LastModifiedUtc = DateTime.UtcNow;
+            }
+
+            return entity;
         }
     }
 }
