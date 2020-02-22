23e29439c8cd16161f64ac2e493993bf83a47940
Musta Mohamed <mustafa.mohamed.wd@gmail.com>
2020-02-22 17:21:14 +0200

    fix deployment path for react

--- WeeksPlanning.Spa/Startup.cs
+++ WeeksPlanning.Spa/Startup.cs
@@ -33,7 +33,10 @@ namespace WeeksPlanning.Spa
             services.AddApplicationValidationServices();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
